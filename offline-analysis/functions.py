4b5b4fc9f93029d855ef38d54e458fc4ed4c2cc0
Fabian Stiehle <f.stiehle@gmail.com>
2021-06-24 12:52:17 +0200

    refactor

--- offline-analysis/functions.py
+++ offline-analysis/functions.py
@@ -1,7 +1,17 @@
 import statistics as stat
-from sklearn.metrics import mean_absolute_error, median_absolute_error
+from sklearn.metrics import mean_absolute_error, median_absolute_error, r2_score
 import pandas as pd
 import numpy as np
+import seaborn as sns
+
+def calculate_error(real, predicted):
+    print('MEAN aboslute error [ms]: ', mean_absolute_error(real, predicted) / 1000000)
+    print('MAX error [ms]: ', max_error(real, predicted) / 1000000)
+    print('MEAN relative (%) error: ', mean_absolute_error_percent(real, predicted))
+   # print('Median aboslute error [ms]: ', median_absolute_error(real, predicted) / 1000000)
+   #print('Median rleative (%) error: ', median_absolute_error_percent(real, predicted))
+    #print('Mean Squared error:', mean_squared_error(real, predicted) / 1000000)
+    print('R2: ', r2_score(real, predicted))    
 
 def group_block(timings, block_index='BLOCK', time_index='NS'):
   timings[time_index + '_median'] = timings[time_index].groupby(timings[block_index]).transform('median')
@@ -46,12 +56,22 @@ def median_absolute_error_percent(y_true, y_pred):
 def adj_r2_score(model,y,yhat):
   """Adjusted R square — put fitted linear model, y value, estimated y value in order
   
-    Example:
-    In [142]: metrics.r2_score(diabetes_y_train,yhat)
-    Out[142]: 0.51222621477934993
+  Example:
+  In [142]: r2_score(diabetes_y_train,yhat)
+  Out[142]: 0.51222621477934993
 
-    In [144]: adj_r2_score(lm,diabetes_y_train,yhat)
-    Out[144]: 0.50035823946984515"""
-  from sklearn import metrics
-  adj = 1 - float(len(y)-1)/(len(y)-len(model.coef_)-1)*(1 - metrics.r2_score(y,yhat))
+  In [144]: adj_r2_score(lm,diabetes_y_train,yhat)
+  Out[144]: 0.50035823946984515"""
+  adj = 1 - float(len(y)-1)/(len(y)-len(model.coef_)-1)*(1 - r2_score(y,yhat))
   return adj
+
+def prepare_plots():
+  # Prepare plots
+  # Color from: https://personal.sron.nl/~pault/
+  colors = ["#332288", "#88CCEE", "#44AA99", "#117733", "#999933", "#DDCC77", "#CC6677", "#882255", "#AA4499"]
+  sns.set(style="whitegrid", color_codes=True, rc={'figure.figsize':(11.7,8.27)}, font_scale=1.4)
+  sns.set_palette(sns.color_palette(colors))
+  #sns.despine(left=True)
+  color = sns.color_palette(sns.color_palette(colors), 9)
+  sns.palplot(sns.color_palette(sns.color_palette(colors), 9))
+  return color
